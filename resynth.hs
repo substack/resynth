46a5b8184469d4ca3e82fd45283cbf57792281fd
James Halliday <substack@gmail.com>
2010-01-19 16:31:21 -0900

    smooth-sounding continuous frequency shifts, but not yet rolled into microphone input

--- resynth.hs
+++ resynth.hs
@@ -6,41 +6,56 @@ import Synthesizer.Plain.Play (monoToInt16)
 
 import Foreign (Ptr, Storable, mallocArray, peekArray)
 import Control.Applicative ((<$>))
-import Control.Monad (forever, unless, when)
+import Control.Monad (forever, unless, when, forM_)
 import Data.Array (listArray)
 
 import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan
 
 main :: IO ()
 main = do
-    let sampleRate = 44100
-    micFreq 8000 1000 $ \freq volume -> do
-        when (volume > 2.1e-5) $ do
-            forkIO $ playFreq 44100 0.25 freq
-            print (freq, volume)
+    fx <- mic 16000 2000
+    forM_ fx $ \(freq,volume) -> do
+        print (freq,volume)
+    
+    {-
+    when (volume > 0.3) $ do
+        forkIO $ playFreq 44100 0.1 freq
+        print (freq, volume)
+    -}
 
-micFreq :: Int -> Int -> (Double -> Double -> IO ()) -> IO ()
-micFreq sampleRate samples handler = do
+mic :: Int -> Int -> IO [(Double,Double)]
+mic sampleRate samples = do
     let
         source = alsaSoundSource "plughw:0,0" soundFormat
         soundFormat = SoundFmt sampleRate :: SoundFmt Double
     buf <- mallocArray samples
     
-    withSoundSource source $ \handle -> forever $ do
+    -- spawn and populate a channel
+    chan <- newChan
+    forkIO $ withSoundSource source $ \handle -> forever $ do
         n <- soundSourceRead source handle buf samples
         rawSound <- peekArray n buf
-        
         let
             volume = (sum $ map abs rawSound) / fromIntegral n
             hz' = pisarenko (listArray (0,n-1) rawSound)
-            hz = hz' * 90 * (fromIntegral sampleRate) / (fromIntegral samples)
-        
-        unless (isNaN hz) $ handler hz volume
+            hz = hz' * 2.82e7 / fromIntegral sampleRate
+        unless (isNaN hz) $ writeChan chan (hz,volume)
+    
+    -- lazily read back everything from the channel
+    getChanContents chan
 
-playFreq :: Int -> Double -> Double -> IO ()
-playFreq sampleRate duration freq =
-    monoToInt16 rate sineWave >> return ()
+playFreqs :: Int -> Double -> [Double] -> IO ()
+playFreqs sampleRate duration freqs =
+    monoToInt16 rate waves >> return ()
     where
-        sineWave = take (floor $ duration * rate)
-            $ map sin [ 0, freq * 2 * pi / rate ..]
+        waves = concatMap (sineWave rate duration) freqs
         rate = fromIntegral sampleRate
+
+sineWave :: Double -> Double -> Double -> [Double]
+sineWave rate duration freq = concat
+    $ take n
+    $ repeat $ map sin [ 0, dt .. 2 * pi ]
+    where
+        dt = freq * 2 * pi / rate
+        n = ceiling $ duration * freq
